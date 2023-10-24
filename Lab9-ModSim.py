113c92ce9d3de3657ea1a7800e282b7b0160ce1c
Cristian Aguirre <107177715+caguirre5@users.noreply.github.com>
2023-10-24 17:58:08 -0600

    Terminado

--- Lab9-ModSim.py
+++ Lab9-ModSim.py
@@ -22,11 +22,15 @@ green = (0, 180, 0)
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Simulación de Robot en el Mundial de Soccer")
 
+# Configurar el reloj
+clock = pygame.time.Clock()
+target_fps = 45  # Establecer el número de FPS deseados
+
 # Posición inicial del robot
 robot_x = 700
 robot_y = 300
 # Posición inicial de la pelota
-pelota_x = 300
+pelota_x = 500
 pelota_y = 200
 # Posición inicial de la porteria objetivo
 porteria_x = 50
@@ -38,23 +42,23 @@ porteria_y = 300
 #Variables CRISP
 distancia = ctrl.Antecedent(np.arange(0, 701, 1), 'distancia')
 # Definir las funciones de membresía para distancia
-distancia['cerca'] = fuzz.trimf(distancia.universe, [0, 150, 300])
-distancia['media'] = fuzz.trimf(distancia.universe, [150, 300, 450])
-distancia['lejos'] = fuzz.trimf(distancia.universe, [300, 500, 700])
+distancia['cerca'] = fuzz.trimf(distancia.universe, [0, 125, 250])
+distancia['media'] = fuzz.trimf(distancia.universe, [240, 370, 500])
+distancia['lejos'] = fuzz.trimf(distancia.universe, [480, 590, 700])
 
 
 resistencia = ctrl.Antecedent(np.arange(0, 101, 1), 'resistencia')
 # Definir las funciones de membresía para distancia
-resistencia['baja'] = fuzz.trimf(resistencia.universe, [0, 25, 50])
-resistencia['normal'] = fuzz.trimf(resistencia.universe, [0, 25, 50])
-resistencia['alta'] = fuzz.trimf(resistencia.universe,[0, 25, 50])
+resistencia['baja'] = fuzz.trimf(resistencia.universe, [0, 17, 34])
+resistencia['normal'] = fuzz.trimf(resistencia.universe, [30, 50, 70])
+resistencia['alta'] = fuzz.trimf(resistencia.universe,[66, 83, 100])
 
 # Definir la variable de salida
-movimiento = ctrl.Consequent(np.arange(0.1, 0.4, 0.1), 'movimiento')
+movimiento = ctrl.Consequent(np.arange(0, 101, 1), 'movimiento')
 # Definir las etiquetas lingüísticas y funciones de membresía
-movimiento['caminar'] = fuzz.trimf(movimiento.universe, [0.1, 0.1, 0.2])
-movimiento['trotar'] = fuzz.trimf(movimiento.universe, [0.1, 0.2, 0.3])
-movimiento['correr'] = fuzz.trimf(movimiento.universe, [0.2, 0.3, 0.3])
+movimiento['caminar'] = fuzz.trimf(movimiento.universe, [0, 17, 34])
+movimiento['trotar'] = fuzz.trimf(movimiento.universe, [30, 50, 70])
+movimiento['correr'] = fuzz.trimf(movimiento.universe, [66, 83, 100])
 
 #Clausulas de Horn / Definir las reglas difusas
 regla1 = ctrl.Rule(distancia['cerca'] & resistencia['baja'], movimiento['caminar'])
@@ -78,25 +82,24 @@ sistema = ctrl.ControlSystemSimulation(sistema_de_control)
 #Variables CRISP
 distancia_porteria = ctrl.Antecedent(np.arange(0, 701, 1), 'distancia_porteria')
 # Definir las funciones de membresía para distancia
-distancia_porteria['cerca'] = fuzz.trimf(distancia.universe, [0, 150, 300])
-distancia_porteria['media'] = fuzz.trimf(distancia.universe, [150, 300, 450])
-distancia_porteria['lejos'] = fuzz.trimf(distancia.universe, [300, 500, 700])
+distancia_porteria['cerca'] = fuzz.trimf(distancia_porteria.universe, [0, 120, 250])
+distancia_porteria['media'] = fuzz.trimf(distancia_porteria.universe, [240, 360, 500])
+distancia_porteria['lejos'] = fuzz.trimf(distancia_porteria.universe, [480, 600, 700])
 
 
 angulo = ctrl.Antecedent(np.arange(-180, 181, 1), 'angulo')
-
 # Definir las funciones de membresía para la variable angulo
-angulo['izquierda'] = fuzz.trimf(angulo.universe, [-180, -90, 0])
-angulo['centro'] = fuzz.trimf(angulo.universe, [-45, 0, 45])
-angulo['derecha'] = fuzz.trimf(angulo.universe, [0, 90, 180])
+angulo['izquierda'] = fuzz.trimf(angulo.universe, [-180, -120, -60])
+angulo['centro'] = fuzz.trimf(angulo.universe, [-60, 0, 60])
+angulo['derecha'] = fuzz.trimf(angulo.universe, [60, 120, 180])
 
 
 # Definir la variable de salida
 fuerza = ctrl.Consequent(np.arange(0, 101, 1), 'fuerza')
 # Definir las etiquetas lingüísticas y funciones de membresía
-fuerza['suave'] = fuzz.trimf(fuerza.universe, [1, 25, 50])
-fuerza['medio'] = fuzz.trimf(fuerza.universe, [25, 50, 75])
-fuerza['fuerte'] = fuzz.trimf(fuerza.universe, [50, 75, 100])
+fuerza['suave'] = fuzz.trimf(fuerza.universe, [0, 17, 34])
+fuerza['medio'] = fuzz.trimf(fuerza.universe, [30, 50, 70])
+fuerza['fuerte'] = fuzz.trimf(fuerza.universe, [66, 83, 100])
 
 
 #Clausulas de Horn / Definir las reglas difusas
@@ -114,17 +117,9 @@ regla9 = ctrl.Rule(distancia_porteria['lejos'] & angulo['derecha'], fuerza['fuer
 sistema_de_control2 = ctrl.ControlSystem([regla1, regla2, regla3, regla4, regla5, regla6, regla7, regla8, regla9])
 sistema2 = ctrl.ControlSystemSimulation(sistema_de_control2)
 
-# Como conocemos las coordenadas de la porteria y pelota, podemos definir la fuerza a la que patearemos desde un inicio
-distancia_porteria_pelota_inicial = ((pelota_x - porteria_x) ** 2 + (pelota_y - porteria_y) ** 2) ** 0.5
-# Calcular el ángulo entre la porteria y la pelota
-angulo_porteria = math.atan2(porteria_y - pelota_y, porteria_x - pelota_x)
+fuerza_des = 0
+fuerza_text = ''
 
-# Definir valores de entrada
-sistema2.input['distancia_porteria'] = distancia_porteria_pelota_inicial
-sistema2.input['angulo'] = angulo_porteria
-
-# Calcular la salida difusa
-sistema2.compute()
 #--------------------------------------------------------------------------------------------------------
 
 # Crear un objeto de fuente
@@ -157,7 +152,7 @@ while running:
 
     # Definir valores de entrada
     sistema.input['distancia'] = distancia_robot_pelota
-    sistema.input['resistencia'] = 10
+    sistema.input['resistencia'] = 20
 
     # Calcular la salida difusa
     sistema.compute()   
@@ -184,10 +179,36 @@ while running:
     # Mostrar el texto en la pantalla
     screen.blit(texto, (550, 10))
 
-    
+    # Renderizar el texto
+    texto2 = font.render(f"Fuerza de pateo: {fuerza_text}", True, (0, 0, 0))
+    # Mostrar el texto en la pantalla
+    screen.blit(texto2, (10, 10))
 
     if distancia_robot_pelota > 18 and not kicked:
-        
+
+        # Como conocemos las coordenadas de la porteria y pelota, podemos definir la fuerza a la que patearemos desde un inicio
+        distancia_porteria_pelota_inicial = ((pelota_x - porteria_x) ** 2 + (pelota_y - porteria_y) ** 2) ** 0.5
+        # Calcular el ángulo entre la porteria y la pelota
+        angulo_porteria = math.atan2(porteria_y - pelota_y, porteria_x - pelota_x)
+
+        # Definir valores de entrada
+        sistema2.input['distancia_porteria'] = distancia_porteria_pelota_inicial
+        sistema2.input['angulo'] = angulo_porteria
+
+        # Calcular la salida difusa
+        sistema2.compute()
+
+        if sistema2.output['fuerza'] > 66:
+            fuerza_des = 20
+            fuerza_text = 'Fuerte'
+        elif sistema2.output['fuerza'] > 33:
+            fuerza_des = 15
+            fuerza_text = 'Medio'
+        else:
+            fuerza_des = 10
+            fuerza_text = 'Suave'   
+
+
         # Calcular las nuevas coordenadas del robot
         nuevo_x = robot_x + desplazamiento * math.cos(angulo)
         nuevo_y = robot_y + desplazamiento * math.sin(angulo)
@@ -201,24 +222,30 @@ while running:
 
     
     if can_kick:
-        # Renderizar el texto
-        texto2 = font.render(f"Valor de fuerza: {sistema2.output['fuerza']:.0f}%", True, (0, 0, 0))
-        # Mostrar el texto en la pantalla
-        screen.blit(texto2, (10, 10))
+        
         
         # Como conocemos las coordenadas de la porteria y pelota, podemos definir la fuerza a la que patearemos desde un inicio
-        distancia_porteria_pelota = ((pelota_x - porteria_x) ** 2 + (pelota_y - porteria_y) ** 2) ** 0.5    
+        distancia_porteria_pelota = ((pelota_x - porteria_x) ** 2 + (pelota_y - porteria_y) ** 2) ** 0.5 
+
+        
 
         if distancia_porteria_pelota > 18:
             # Calcular las nuevas coordenadas del robot
-            nuevo_x = pelota_x + (sistema2.output['fuerza']/100) * math.cos(angulo_porteria)
-            nuevo_y = pelota_y + (sistema2.output['fuerza']/100) * math.sin(angulo_porteria)
+            nuevo_x = pelota_x + fuerza_des * math.cos(angulo_porteria)
+            nuevo_y = pelota_y + fuerza_des * math.sin(angulo_porteria)
 
             # Actualizar la posición del robot
             pelota_x = nuevo_x
             pelota_y = nuevo_y 
 
+            fuerza_des -= 1
+
+            if fuerza_des == 0:# and (pelota_x != porteria_y) and (pelota_y != porteria_x)
+                can_kick = False
+                kicked = False
+
     pygame.display.update()
+    clock.tick(target_fps)
 # Salir de Pygame
 pygame.quit()
 sys.exit()
